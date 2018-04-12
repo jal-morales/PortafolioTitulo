b818fe3a2be9a83523007a455887b7c7b6830954
Javier Antonio Lopez Morales <jal.morales89@gmail.com>
2018-04-11 23:21:26 -0300

    Se realiza Clase de Conexion, se realiza prueba de conexion a base de dato oracle 11g quedando funcional 100% operativa.

--- src/java/Conexion/ConexionBD.java
+++ src/java/Conexion/ConexionBD.java
@@ -5,11 +5,30 @@
  */
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import oracle.jdbc.OracleDriver;
+
 /**
  *
  * @author JavierL.M
  */
 public class ConexionBD 
 {
+
     
+    //METODO QUE REALIZA LA CONEXION
+    public Connection getConnection() throws SQLException, 
+            ClassNotFoundException 
+    {
+        
+        DriverManager.registerDriver(new OracleDriver());
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","DONACLARITA","system");
+        System.out.println("¡Conexión Exitosa!");
+        return conn;
+    }
+
 }
