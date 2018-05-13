05d07c0c8eb827341ad841bb4f99816044644c39
darsay8 <darsay.one@gmail.com>
2018-05-13 13:34:11 -0400

    Actualización archivo functions.js

--- web/js/functions.js
+++ web/js/functions.js
@@ -5,19 +5,55 @@ $(document).ready(function () {
     var hash = this.hash;
     $('html, body').animate({
       scrollTop: $(hash).offset().top
-    }, 800, function(){
+    }, 600, function(){
       window.location.hash = hash;
     });
   } 
 });
- if($(window).innerWidth() <= 768) {
-   $(".navbar").css('background-color', 'rgb(130, 93, 89)');
- }
 });
-window.sr = ScrollReveal();
-sr.reveal('.navbar-brand', { duration: 2000, origin: 'top' });
-sr.reveal('.navbar-nav', { duration: 2000, origin: 'top' });
-sr.reveal('.tthb', { duration: 1500, origin: 'top' });
-sr.reveal('.imghb0', { duration: 2000, origin: 'bottom' });
-sr.reveal('.imghb1', { duration: 2200, origin: 'bottom' });
-sr.reveal('.imghb2', { duration: 2400, origin: 'bottom' });
+
+if($(window).innerWidth() >= 768) {
+  window.sr = ScrollReveal();
+  sr.reveal('.navbar-brand', { duration: 2000, origin: 'top' });
+  sr.reveal('.navbar-nav', { duration: 2000, origin: 'top' });
+  sr.reveal('.h2r', { duration: 1500, origin: 'top' });
+  sr.reveal('.imsr0', { duration: 2000, origin: 'bottom' });
+  sr.reveal('.imsr1', { duration: 2200, origin: 'bottom' });
+  sr.reveal('.imsr2', { duration: 2400, origin: 'bottom' });
+sr.reveal('.imgr1', { duration: 2000, origin: 'bottom' });
+ sr.reveal('.imgr2', { duration: 2200, origin: 'bottom' });
+ sr.reveal('.imgr3', { duration: 2400, origin: 'bottom' });
+ sr.reveal('.imgr4', { duration: 2600, origin: 'bottom' });
+}else {
+ window.sr = ScrollReveal();
+ sr.reveal('.h2r', { duration: 1500, origin: 'top' });
+ sr.reveal('.imsr0', { duration: 2000, origin: 'bottom' });
+ sr.reveal('.imsr1', { duration: 2200, origin: 'bottom' });
+ sr.reveal('.imsr2', { duration: 2400, origin: 'bottom' });
+ sr.reveal('.imgr1', { duration: 2000, origin: 'bottom' });
+ sr.reveal('.imgr2', { duration: 2200, origin: 'bottom' });
+ sr.reveal('.imgr3', { duration: 2400, origin: 'bottom' });
+ sr.reveal('.imgr4', { duration: 2600, origin: 'bottom' });
+
+}
+
+$('.carousel').carousel({
+  interval: 4000
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
